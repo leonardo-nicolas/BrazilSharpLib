b467b2eefcb1a11b498147c91de67aad3b56e945
leonardon397 <leonardon397@hotmail.com>
2022-01-10 00:53:11 -0300

    Made refactors and add method to Validate Credit Card issued in BR, but, there is a TODO for ELO brazilian card

---
+++ Source/BrazilianStates.cs
@@ -0,0 +1,63 @@
+namespace BrazilSharp
+{
+    /// <summary>All Brazilian States. Its value is the Brazilian IBGE state's code.</summary>
+    public enum BrazilianStates
+    {
+        /// <summary>Acre</summary>
+        AC = 12,
+        /// <summary>Alagoas</summary>
+        AL = 27,
+        /// <summary>Amapá</summary>
+        AP = 16,
+        /// <summary>Amazonas</summary>
+        AM = 13,
+        /// <summary>Bahia</summary>
+        BA = 29,
+        /// <summary>Ceará</summary>
+        CE = 23,
+        /// <summary>Federal District (PT-BR: Distrito Federal)</summary>
+        DF = 53,
+        /// <summary>Espírito Santo</summary>
+        ES = 32,
+        /// <summary>Goiás</summary>
+        GO = 52,
+        /// <summary>Maranhão</summary>
+        MA = 21,
+        /// <summary>Mato Grosso</summary>
+        MT = 51,
+        /// <summary>Mato Grosso do Sul</summary>
+        MS = 50,
+        /// <summary>Minas Gerais</summary>
+        MG = 31,
+        /// <summary>Pará</summary>
+        PA = 15,
+        /// <summary>Paraíba</summary>
+        PB = 25,
+        /// <summary>Paraná</summary>
+        PR = 41,
+        /// <summary>Pernambuco</summary>
+        PE = 26,
+        /// <summary>Piauí</summary>
+        PI = 22,
+        /// <summary>Rio Grande do Norte</summary>
+        RN = 24,
+        /// <summary>Rio Grande do Sul</summary>
+        RS = 43,
+        /// <summary>Rio de Janeiro</summary>
+        RJ = 33,
+        /// <summary>Rondônia</summary>
+        RO = 11,
+        /// <summary>Roraima</summary>
+        RR = 14,
+        /// <summary>Santa Catarina</summary>
+        SC = 42,
+        /// <summary>São Paulo</summary>
+        SP = 35,
+        /// <summary>Sergipe</summary>
+        SE = 28,
+        /// <summary>Tocantins</summary>
+        TO = 17,
+        /// <summary>Other unknown state or Out of Brazil. The code is unofficial from Brazilian IBGE state's Code.</summary>
+        ZZ = 99
+    }
+}
---
+++ Source/Calculations/CalculationsEnuns.cs
@@ -0,0 +1,10 @@
+namespace BrazilSharp.Calculations
+{
+    /// <summary>Period Indicator for Calculations</summary>
+    public enum PeriodType{
+        /// <summary>Calculate based on days</summary>
+        Day,
+        /// <summary>Calculate based on months</summary>
+        Month
+    }
+}
